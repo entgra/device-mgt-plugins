27182007eb17fedf5f5a58198d946819e0247c7e
Saad Sahibjan <saadsahibjan@gmail.com>
2019-04-02 13:45:33 +0530

    Change single mode app variable name

--- components/mobile-plugins/android-plugin/org.wso2.carbon.device.mgt.mobile.android.ui/src/main/resources/jaggeryapps/devicemgt/app/units/cdmf.unit.device.type.android.operation-mod/public/js/operation-mod.js
+++ components/mobile-plugins/android-plugin/org.wso2.carbon.device.mgt.mobile.android.ui/src/main/resources/jaggeryapps/devicemgt/app/units/cdmf.unit.device.type.android.operation-mod/public/js/operation-mod.js
@@ -210,7 +210,7 @@ var androidOperationModule = function () {
                     payload["kioskBackgroundImage"] = deviceGlobalConfigurations["kioskBackgroundImage"];
                     payload["kioskLogoImage"] = deviceGlobalConfigurations["kioskLogoImage"];
                     payload["kioskAppName"] = deviceGlobalConfigurations["kioskAppName"];
-                    payload["isSingleAppMode"] = deviceGlobalConfigurations["isSingleAppMode"];
+                    payload["isSingleModeApp"] = deviceGlobalConfigurations["isSingleModeApp"];
                     payload["isIdleGraphicsEnabled"] = deviceGlobalConfigurations["isIdleGraphicsEnabled"];
                     payload["idleTimeout"] = deviceGlobalConfigurations["idleTimeout"];
                     payload["isMultiUserDevice"] = deviceGlobalConfigurations["isMultiUserDevice"];
@@ -502,17 +502,17 @@ var androidOperationModule = function () {
                      if (operationData["kioskAppName"]) {
                          deviceGlobalConfigurations["kioskAppName"] = operationData["kioskAppName"];
                      }
-                     deviceGlobalConfigurations["isSingleAppMode"] = operationData["isSingleAppMode"];
-                     if (deviceGlobalConfigurations["isSingleAppMode"] === true) {
+                     deviceGlobalConfigurations["isSingleModeApp"] = operationData["isSingleModeApp"];
+                     if (deviceGlobalConfigurations["isSingleModeApp"] === true) {
                          var selectedSingleModeAppElement = "div#install-app-enrollment .child-input";
                          if ($(selectedSingleModeAppElement + "[data-child-key='type']").val() === "Web Clip") {
                              var webClipApp = {
                                  "identity": $(selectedSingleModeAppElement + "[data-child-key='packageName']").val(),
                                  "title": $(selectedSingleModeAppElement + "[data-child-key='appName']").val()
                              };
-                             deviceGlobalConfigurations["singleApplicationModeApp"] = JSON.stringify(webClipApp);
+                             deviceGlobalConfigurations["singleModeApp"] = JSON.stringify(webClipApp);
                          } else {
-                             deviceGlobalConfigurations["singleApplicationModeApp"]
+                             deviceGlobalConfigurations["singleModeApp"]
                                  = $(selectedSingleModeAppElement + "[data-child-key='packageName']").val();
                          }
                      }
