cf9a527b5a73c027ca13123648fd78222b617bd5
Dhishan <dhishan.dra@gmail.com>
2025-10-14 08:14:22 +0530

    update access token saving to only username for connections

--- components/extensions/cdmf-transport-adapters/output/io.entgra.device.mgt.plugins.output.adapter.mqtt/src/main/java/io/entgra/device/mgt/plugins/output/adapter/mqtt/util/MQTTAdapterPublisher.java
+++ components/extensions/cdmf-transport-adapters/output/io.entgra.device.mgt.plugins.output.adapter.mqtt/src/main/java/io/entgra/device/mgt/plugins/output/adapter/mqtt/util/MQTTAdapterPublisher.java
@@ -39,8 +39,6 @@ import org.wso2.carbon.event.output.adapter.core.exception.ConnectionUnavailable
 import org.wso2.carbon.event.output.adapter.core.exception.OutputEventAdapterException;
 import org.wso2.carbon.event.output.adapter.core.exception.OutputEventAdapterRuntimeException;
 
-import static io.entgra.device.mgt.plugins.output.adapter.mqtt.util.MQTTEventAdapterConstants.TOKEN_SPLIT_INDEX;
-
 /**
  * MQTT publisher related configuration initialization and publishing capabilties are implemented here.
  */
@@ -79,8 +77,7 @@ public class MQTTAdapterPublisher {
             connectionOptions.setKeepAliveInterval(keepAlive);
             if (mqttBrokerConnectionConfiguration.getUsername() != null) {
                 String accessToken = getToken();
-                connectionOptions.setUserName(accessToken.substring(0, TOKEN_SPLIT_INDEX));
-                connectionOptions.setPassword(accessToken.substring(TOKEN_SPLIT_INDEX).toCharArray());
+                connectionOptions.setUserName(accessToken);
             }
             // Construct an MQTT blocking mode client
             mqttClient = new MqttClient(mqttBrokerConnectionConfiguration.getBrokerUrl(), clientId, dataStore);
